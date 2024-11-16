93638ff2f1e1b379b52bf69bc5e7bd3690577f53
Dan <22007659+dansenpir@users.noreply.github.com>
2024-11-16 12:36:56 -0300

    build: change from 'maven' to 'gradle'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
-rootProject.name = "auth"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "user"
+include("core")
+include("usecase")
+include("application")
+include("infrastructure")
